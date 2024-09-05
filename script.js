974850c8879f7de0510dc0a23d29b17cdd778704
PedroSalomaoR <160697967+PedroSalomaoR@users.noreply.github.com>
2024-09-05 20:41:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+function nextSlide() {
+  currentIndex++;
+  if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  }
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 2000);
